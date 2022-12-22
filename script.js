c284c130a385b17c2338ea9bacbdd87503c9dcd2
fernandojosecc <fernandojosecc@hotmail.com>
2022-12-22 14:50:31 -0800

    Eventos en JavaScript: Interactuando con usuarios

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.getElementsByClassName('.parrafito');
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
+const h1= document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
 
-console.log(input.value);
-
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input,
-});
-
-h1.innerHTML = 'Patito <br> FEO'; //Con esto podemos modificar lo que queremos que muestre el HTML
-h1.innerText = 'Patito <br> FEO'; //Con esto podemos modificar lo que queremos que aparezca en la etiqueta como texto.
-// console.log(h1.getAttribute('pantalla')); //Manda a llamar el atributo pantalla en el HTML
-// h1.setAttribute('pantalla','rojo');//Cambia lo que tenga el atributo adentro
-
-h1.classList.add('rojo');
-h1.classList.remove('verde');
-h1.classList.toggle('verde');
-h1.classList.contains('verde');
-
-input.value = "456";
-
-const img = document.createElement('img');
-img.setAttribute('src', 'https://media2.giphy.com/media/p092OM3vVCXII/giphy.webp');
-console.log(img);
-
-pid.append(img);
+function btnOnClick(){ 
+    const sumaInputs = Number(input1.value) + parseInt(input2.value); //Ambos Number y parseInt sirven para transformar los valores de string a numero
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
